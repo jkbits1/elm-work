4c5d4b2b3d5bba8ed4542ae70fbe2ce9001cf330
jkbits1 <jkbits1@users.noreply.github.com>
2015-05-11 01:04:35 +0100

    added code so button is displayed

--- channelCreate.elm
+++ channelCreate.elm
@@ -2,15 +2,22 @@
 
 --import Signal (channel)
 import Signal 
-import Html
+--import Html
+import Html exposing (Html, div)
+import Html.Attributes exposing (class)
 import Html.Events
+import Graphics.Input.Field as Field
+import Graphics.Element exposing (..)
+
+-- from Elm Architecture tutorial, but suitable here too
+type alias Model = Int
 
 type Update = NoOp | Add Int | Remove Int
 
 updatesChnl : Signal.Mailbox Update
 updatesChnl = Signal.mailbox NoOp
 
-addButton : Html.Html
+addButton : Html
 addButton = Html.button
 --  [ Html.Events.onClick (Signal.send updates (Add 1))]
   [ Html.Events.onClick updatesChnl.address (Add 1)]
@@ -21,3 +28,35 @@ addButton = Html.button
 --main = Signal.map 
 --  (view updates)
 --  (foldp step initialState (subscribe updates))
+
+-- from text field example
+
+--view : Field.Content -> Element
+--view fieldContent =
+--  flow down
+--  [ addButton ]
+  
+--main = div [class "container"]
+--  [
+--    addButton
+--  ]
+
+main : Signal Html
+main = Signal.map (view updatesChnl.address) model
+
+view : Signal.Address Update -> Model -> Html
+view address model = div [class "container"]
+  [
+    addButton
+  ]
+
+model : Signal Model
+model = Signal.foldp updateModel 0 updatesChnl.signal 
+
+updateModel : Update -> Model -> Model
+updateModel update model =
+  case update of
+    NoOp        -> model 
+    Add val     -> model + 1    
+    Remove val  -> model - 1
+    
