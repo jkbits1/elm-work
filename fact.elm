1f2c6401bd5b345486143b9c6042d62c7d31cdfb
jkbits1 <jkbits1@users.noreply.github.com>
2016-07-15 05:21:29 +0100

    test leng fn

--- fact.elm
+++ fact.elm
@@ -1,12 +1,26 @@
-import Graphics.Element (Element)
-import Text (asText)
+import Html exposing (..)
+import Html.App as HtmlApp
+import Html.Attributes as Attr exposing (..)
+import Html.Events exposing (..)
+import Text exposing (color)
+import Color exposing (..)
+
+import String
+import List exposing (..)
+-- import Graphics.Element (Element)
+-- import Text (asText)
 
 factorial : Int -> Int
 factorial n =
   if n <= 1
     then 1
     else n * factorial (n-1)
+    
+leng xs = 
+        case xs of
+            [] -> 0
+            x :: xs -> 1 + leng xs
 
-main : Element
+-- main : Element
 main =
-  asText (factorial 5)
+  text <| toString (factorial 5)
